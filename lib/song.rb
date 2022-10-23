edc25bb4c6c8bfd2f7e38ee9a644b42dee9ebb5c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-23 17:55:29 +0300

    Finished Lab!

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 require 'pry'
+require_relative './concerns/memorable'
+require_relative './concerns/findable'
+require_relative './concerns/paramable'
 
 class Song
   attr_accessor :name
   attr_reader :artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  extend Paramable 
 
   @@songs = []
 
@@ -10,20 +16,8 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
